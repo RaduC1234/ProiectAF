b27457eeea6b461bc8d0a30f7f695fde6a88871d
Radu <raduandreicapaatina@yahoo.com>
2023-12-18 14:53:08 +0100

    .

---
+++ src/main/java/binarySearch/ParallelBinarySearch.java
@@ -0,0 +1,61 @@
+package binarySearch;
+
+import java.util.List;
+import java.util.concurrent.*;
+
+public class ParallelBinarySearch<T extends Comparable<T>> {
+
+    private final List<T> array;
+    private final T target;
+    private final ExecutorService service;
+
+    public ParallelBinarySearch(List<T> array, T target, int threadPoolSize) {
+        if (target == null) {
+            throw new IllegalArgumentException("Target cannot be null.");
+        }
+        this.array = array;
+        this.target = target;
+        this.service = Executors.newCachedThreadPool();
+    }
+
+    public int search() throws InterruptedException, ExecutionException {
+        try {
+            Future<Integer> result = service.submit(() -> binarySearch(0, array.size() - 1));
+            return result.get();
+        } finally {
+            service.shutdown();
+            try {
+                if (!service.awaitTermination(5, TimeUnit.SECONDS)) {
+                    service.shutdownNow();
+                }
+            } catch (InterruptedException e) {
+                service.shutdownNow();
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    private int binarySearch(int startIndex, int endIndex) throws InterruptedException, ExecutionException {
+        if (startIndex <= endIndex) {
+            int mid = startIndex + (endIndex - startIndex) / 2; // int overflow protection
+
+            if (array.get(mid).equals(target)) {
+                return mid;
+            } else {
+                Future<Integer> leftFuture = service.submit(() -> binarySearch(startIndex, mid - 1));
+                Future<Integer> rightFuture = service.submit(() -> binarySearch(mid + 1, endIndex));
+
+                int leftResult = leftFuture.get();
+                int rightResult = rightFuture.get();
+
+                if (leftResult != -1) {
+                    return leftResult;
+                } else if (rightResult != -1) {
+                    return rightResult;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
